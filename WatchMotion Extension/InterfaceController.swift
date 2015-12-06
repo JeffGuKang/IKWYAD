3dba60ba2110c186f89fe7190966df8578010a13
Jeff Kang <sugar1230@gmail.com>
2015-12-06 22:17:28 +0900

    - Working to add accelerometer information for apple watch.

--- WatchMotion Extension/InterfaceController.swift
+++ WatchMotion Extension/InterfaceController.swift
@@ -10,13 +10,18 @@ import WatchKit
 import Foundation
 import CoreMotion
 
+let kUpdateFrequency = 60.0 // min: 10, max: 100
+
 class InterfaceController: WKInterfaceController {
     @IBOutlet var table: WKInterfaceTable!;
     @IBOutlet var accelLabel: WKInterfaceLabel!
     @IBOutlet var gyroLabel: WKInterfaceLabel!
     @IBOutlet var magneticLabel: WKInterfaceLabel!
     @IBOutlet var deviceLabel: WKInterfaceLabel!
-    var motionMgr: CMMotionManager!;
+    
+    var groupController = [TableGroupController]()
+    var motionMgr: CMMotionManager!
+    //    var accelData: CMAccelerometerData?;
     
     override func awakeWithContext(context: AnyObject?) {
         super.awakeWithContext(context)
@@ -30,6 +35,18 @@ class InterfaceController: WKInterfaceController {
         
         if self.motionMgr.accelerometerAvailable {
             self.accelLabel.setTextColor(UIColor.whiteColor())
+            self.motionMgr.accelerometerUpdateInterval = 1/kUpdateFrequency
+            self.groupController.append(TableGroupController(Label: "X"))
+            self.groupController.append(TableGroupController(Label: "Y"))
+            self.groupController.append(TableGroupController(Label: "Z"))
+            print("Accel is working")
+            
+            self.table.setNumberOfRows(self.groupController.count, withRowType: "motionRow")
+            motionMgr.startAccelerometerUpdatesToQueue(NSOperationQueue.mainQueue(), withHandler: { (accelData: CMAccelerometerData?, error: NSError?) -> Void in
+                self.groupController[0].axisValue.setText(String(accelData?.acceleration.x))
+                self.groupController[1].axisValue.setText(String(accelData?.acceleration.y))
+                self.groupController[2].axisValue.setText(String(accelData?.acceleration.z))
+            })
         }
         if self.motionMgr.gyroAvailable {
             self.gyroLabel.setTextColor(UIColor.whiteColor())
@@ -40,21 +57,19 @@ class InterfaceController: WKInterfaceController {
         if self.motionMgr.deviceMotionAvailable {
             self.deviceLabel.setTextColor(UIColor.whiteColor())
         }
-        
-        
     }
-
+    
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
     }
-
+    
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
     }
     
     
-
+    
 }
 
